e676f41f2da8da5a336ef7db8d971b7a2bb7e0e0
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-06-27 19:52:39 +0200

    REMEMBER TO CHECK MEMCCPY

---
+++ gnl_vog_temp/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_get_next_line.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcajee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/18 17:29:40 by tcajee            #+#    #+#             */
+/*   Updated: 2019/06/27 10:49:11 by tcajee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	char	*line = NULL;
+	int x = 1;
+	int fd = open("./test.txt", O_RDONLY);
+
+	if (!fd)
+		return (-99);
+	while (x > 0)
+	{
+		x = get_next_line(fd, &line);
+		printf("[%d] line: %s\n", x, line);
+	}
+	ft_strdel(&line);
+	close(fd);
+	return (0);
+}
