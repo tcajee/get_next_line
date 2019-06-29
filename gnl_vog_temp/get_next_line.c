c8119ea077b2597ece2cbfbba5d37931ce81f101
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-06-29 11:49:44 +0200

    Refactored .c [cleanish]

--- gnl_vog_temp/get_next_line.c
+++ gnl_vog_temp/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 10:15:46 by tcajee            #+#    #+#             */
-/*   Updated: 2019/06/27 18:22:24 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/06/29 11:49:11 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 static int	copy_new_line(t_files *files, int fd)
 {
 	FT_(!(files->stage = ft_strdup(files->file[fd])), -1);
-	ft_strdel(&files->line);
-	FT_(!(files->line = ft_strdup(\
-					ft_memset(ft_strchr(files->stage, '\n'), '\0', 1)\
-					- ft_strlen(files->stage))), -1);
+//	ft_strdel(&files->line);
+	ft_memset(ft_strchr(files->stage, '\n'), '\0', 1);
+	FT_(!(files->line = ft_strdup(files->stage)), -1);
 	ft_strdel(&files->file[fd]);
 	FT_(!(files->file[fd] = ft_strdup(ft_strchr(files->stage, '\0') + 1)), -1);
 	ft_strdel(&files->stage);
@@ -33,20 +32,18 @@ static int	find_new_line(t_files *files, int fd)
 		files->file[fd] = ft_strnew(0);
 	while (ft_strchr(files->file[fd], '\n') == NULL)
 	{
-		FT_(!(files->buffer = \
-					(char *)malloc(sizeof(char) * BUFF_SIZE + 1)), -1);
+		FT_(!(files->buffer = ft_strnew(BUFF_SIZE)), -1);
 		FT_((bytes = read(fd, files->buffer, BUFF_SIZE)) == 0, 0);
 		if (bytes < 0)
 		{
 			ft_strdel(&files->buffer);
 			return (-1);
 		}
-		FT_(!(files->stage = ft_strjoin(files->file[fd],ft_memset(\
-							(files->buffer + bytes), '\0', 1) - bytes)), -1);
+		FT_(!(files->stage = ft_strjoin(files->file[fd], files->buffer)), -1);
 		ft_strdel(&files->file[fd]);
+		ft_strdel(&files->buffer);
 		FT_(!(files->file[fd] = ft_strdup(files->stage)), -1);
 		ft_strdel(&files->stage);
-		ft_strdel(&files->buffer);
 	}
 	return (1);
 }
--- gnl_vog_temp/get_next_line.h
+++ gnl_vog_temp/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 10:17:44 by tcajee            #+#    #+#             */
-/*   Updated: 2019/06/29 11:21:03 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/06/29 11:27:18 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 64
 # include "./libft/libft.h"
 
 typedef struct	s_files
