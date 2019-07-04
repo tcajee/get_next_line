96c97ab7711f987e324049fd8cf7f1d107e05a4e
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-07-04 10:59:34 +0200

    cleaned repo to pull

--- get_next_line/moulinette/test_basic/get_next_line.h
+++ get_next_line/moulinette/test_basic/get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: tcajee <tcajee@student.wethinkcode.co.za>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 10:17:44 by tcajee            #+#    #+#             */
-/*   Updated: 2019/07/03 16:21:24 by tcajee           ###   ########.fr       */
+/*   Updated: 2019/07/03 17:52:44 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 100
+# define BUFF_SIZE 1
 # include "./libft/libft.h"
 
 typedef struct	s_files
 {
-	char		*file[1024];
+	char		*file[1025];
 	char		*line;
 }				t_files;
 
