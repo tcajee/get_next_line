01564c91f26c8e3155865cf02341eb3581ae5b86
Tameem Cajee <tcajee@student.wethinkcode.co.za>
2019-07-06 22:39:54 +0200

    lnkambulish

--- get_next_line/moulinette/test_basic/lnkambul/temp.sh
+++ get_next_line/moulinette/test_basic/lnkambul/temp.sh
@@ -3,10 +3,10 @@
 
 cd ~/Desktop/gnl_git/get_next_line/moulinette/test_basic/test_basic_main
 
-# cp -r * ~/Desktop/gnl_git/get_next_line/moulinette/test_basic/tcajee
+# cp -r * ~/Desktop/gnl_git/get_next_line/moulinette/test_basic/kseperep
 
-cd ~/Desktop/gnl_git/get_next_line/moulinette/test_basic/tcajee
-echo "Testing get_next_line submission by: tcajee"
+cd ~/Desktop/gnl_git/get_next_line/moulinette/test_basic/kseperep
+echo "Testing get_next_line submission by: kseperep"
 read -p "Press enter to continue"
 sh remake.sh
 read -p "Press enter to continue"
