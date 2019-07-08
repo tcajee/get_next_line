8a1c4653bffa4f5cae629c4077175c1a268221a1
Tameem CAJEE <tcajee@c4r16s1.wethinkcode.co.za>
2019-07-08 16:38:52 +0200

    vesingh

--- get_next_line/moulinette/test_hard/get_next_line.c
+++ get_next_line/moulinette/test_hard/get_next_line.c
@@ -6,63 +6,63 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 08:28:55 by vesingh           #+#    #+#             */
-/*   Updated: 2019/07/05 13:06:17 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/07/08 16:38:46 by tcajee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 
-static int ft_storetoline(char **store, char **line)
-{
-	int len;
-	char	*temp;
-	if (ft_strchr(*store, '\n'))
-	{
-		len = ft_strchr(*store, '\n') - *store;
-		*line = ft_strsub(*store, 0, len);
-		temp = ft_strchr(*store, '\n') + 1;
-		temp = ft_strdup(temp);
-		ft_strdel(store);
-		*store = ft_strdupdel(&temp);
-		if (*store[0] == '\0')
-			ft_strdel(store);
-	}
-	else if (ft_strchr(*store, '\0'))
-		*line = ft_strdupdel(store);
-	return (1);
-}
-
-/* static int	ft_storetoline(char **store, char **line) */
+/* static int ft_storetoline(char **store, char **line) */
 /* { */
-/* 	int		len; */
+/* 	int len; */
 /* 	char	*temp; */
 /* 	if (ft_strchr(*store, '\n')) */
 /* 	{ */
-/* 		temp = *store; */
-/* 		len = ft_strchr(temp, '\n') - temp; */
-/* 		*line = ft_strsub(temp, 0, len); */
-/* 		*store = ft_strsub(temp, len + 1, ft_strlen(temp) - len); */
-/* 		ft_strdel(&temp); */
+/* 		len = ft_strchr(*store, '\n') - *store; */
+/* 		*line = ft_strsub(*store, 0, len); */
+/* 		temp = ft_strchr(*store, '\n') + 1; */
+/* 		temp = ft_strdup(temp); */
+/* 		ft_strdel(store); */
+/* 		*store = ft_strdupdel(&temp); */
+/* 		if (*store[0] == '\0') */
+/* 			ft_strdel(store); */
 /* 	} */
-/* 	else if (*store[0] != '\0') */
+/* 	else if (ft_strchr(*store, '\0')) */
 /* 		*line = ft_strdupdel(store); */
 /* 	return (1); */
 /* } */
 
-/* static int	ft_reader(char **store, int fd) */
-/* { */
-/* 	char	buff[BUFF_SIZE + 1]; */
-/* 	int		n; */
-/* 	while (!ft_strchr(*store, '\n')) */
-/* 	{ */
-/* 		if ((n = read(fd, buff, BUFF_SIZE)) == 0) */
-/* 			return (0); */
-/* 		buff[n] = '\0'; */
-/* 		*store = ft_strjoinfree(*store, buff); */
-/* 	} */
-/* 	return (1); */
-/* } */
+static int	ft_storetoline(char **store, char **line)
+{
+	int		len;
+	char	*temp;
+	if (ft_strchr(*store, '\n'))
+	{
+		temp = *store;
+		len = ft_strchr(temp, '\n') - temp;
+		*line = ft_strsub(temp, 0, len);
+		*store = ft_strsub(temp, len + 1, ft_strlen(temp) - len);
+		ft_strdel(&temp);
+	}
+	else if (*store[0] != '\0')
+		*line = ft_strdupdel(store);
+	return (1);
+}
+
+static int	ft_reader(char **store, int fd)
+{
+	char	buff[BUFF_SIZE + 1];
+	int		n;
+	while (!ft_strchr(*store, '\n'))
+	{
+		if ((n = read(fd, buff, BUFF_SIZE)) == 0)
+			return (0);
+		buff[n] = '\0';
+		*store = ft_strjoinfree(*store, buff);
+	}
+	return (n);
+}
 
 static int ft_reader(char **store, const int fd)
 {
